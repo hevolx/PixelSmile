ad1b61dbc200cf364b807e13b18970890bbc6bcd
Love Holmgren <143357495+hevolx@users.noreply.github.com>
2026-04-18 13:54:24 +0200

    feat(image): implement drawDot and fix y coordinate type

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ drawDot(12, 2);
 // smile
 drawDot(4, 4);
 drawHorizontalLine(4, 5, 12);
-drawDot(15, "4");
+drawDot(15, 4);
 
 // output what we drew to the console
 outputImage();
@@ -30,6 +30,16 @@ function drawRectangle(
   drawVerticalLine(x, y, height);
   // right
   drawVerticalLine(x + width - 1, y, height);
+}
+
+
+function drawDot(
+  x: number,
+  y: number
+) {
+  if (isPointInImage(x, y)) {
+    imageData[y * imageWidth + x] = true;
+  }
 }
 
 /**
